0cbe9b4cc781b64cdea5a8e1b44742d3b89235da
Githron <103646138+Githron@users.noreply.github.com>
2022-12-08 05:06:53 +0000

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -1,3 +1,17 @@
 function myFunction() {
     alert("Life is a waterfall\nWe're one in the river\nAnd one again after the fall\nSwimming through the void, we hear the word\nWe lose ourselves, but we find it all\n'Cause we are the ones that want to play\nAlways want to go, but you never want to stay\nAnd we are the ones that want to choose\nAlways want to play but you never want to lose\nAerials in the sky\nWhen you lose small mind, you free your life\nLife is a waterfall\nWe drink from the river\nThen we turn around and put up our walls\nSwimming through the void, we hear the word\nWe lose ourselves, but we find it all\n'Cause we are the ones that want to play\nAlways want to go, but you never want to stay\nAnd we are the ones that want to choose\nAlways want to play but you never want to lose\nOh\nAerials in the sky\nWhen you lose small mind, you free your life\nAerials, so up high\nWhen you free your eyes, eternal prize\nAerials in the sky\nWhen you lose small mind, you free your life\nAerials, so up high\nWhen you free your eyes, eternal prize");
   }
+  
+  
+ 
+            var sound = new Howl({
+            src: ['System Of A Down - Aerials (Official HD Video)(MP3_320K).mp3'],
+            autoplay: true,
+            loop: true,
+            volume: 0.5,
+            onend: function() {
+                console.log('Finished!');
+            }
+            });
+        
+ 
